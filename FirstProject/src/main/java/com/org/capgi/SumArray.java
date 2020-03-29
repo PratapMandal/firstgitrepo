eb985d1f9525417a49028242531679fc3991d0b9
PratapMandal <pratapmandal3264@gmail.com>
2020-03-29 02:18:54 -0700

    Extended sum

--- FirstProject/src/main/java/com/org/capgi/SumArray.java
+++ FirstProject/src/main/java/com/org/capgi/SumArray.java
@@ -4,7 +4,7 @@ public class SumArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {1,2,3,4,5};
+		int arr[]= {1,2,3,4,5,6,7,8,9,10};
 		int sum=0;
 		for(int i=0;i<arr.length;i++) {
 
